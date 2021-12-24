9a52fd55daedb2bc618a4b47a960fa3309d35c43
Huang Robin <robinhuang.ut@gmail.com>
2021-12-24 00:10:31 -0500

    win32print

---
+++ zpl/test.py
@@ -0,0 +1,29 @@
+label = """
+^XA
+
+^FO140,15
+^A0,40,40
+^FD
+Total Weight : 50 KG
+^FS
+
+^FO140,60
+^A0,40,40
+^FD
+shipment_count: 34
+^FS
+
+^FO140,105
+^A0,40,40
+^FD
+HUB: DEL
+^FS
+
+
+^XZ
+"""
+
+# import zebra
+from zebra import Zebra
+z = Zebra('zebra_test')
+z.output(label)
